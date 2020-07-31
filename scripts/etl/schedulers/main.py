d97575d1071c330f93d3b6951c0858f37f0851ad
Kyle Lahnakoski <kyle@lahnakoski.com>
2020-07-31 09:50:29 -0400

    fix, formatting

--- scripts/etl/schedulers/main.py
+++ scripts/etl/schedulers/main.py
@@ -15,26 +15,19 @@ import taskcluster
 
 import adr
 import mo_math
-from adr.sources import SourceHandler
 from adr import configuration as adr_configuration
 from adr.configuration import Configuration, CustomCacheManager
 from adr.errors import MissingDataError
+from adr.sources import SourceHandler
 from adr.util.cache_stores import S3Store
 from jx_bigquery import bigquery
 from jx_python import jx
 from logs import capture_logging, capture_loguru
-from mo_dots import (
-    Data,
-    coalesce,
-    listwrap,
-    concat_field,
-    set_default,
-    to_data)
+from mo_dots import Data, coalesce, listwrap, concat_field, set_default, to_data
 from mo_http import http
 from mo_json import value2json, json2value
 from mo_logs import startup, constants, Log
 from mo_threads import Process, Till
-from mo_threads.repeat import Repeat
 from mo_times import Date, Duration, Timer, MINUTE
 from mozci.push import make_push_objects
 from pyLibrary.env import git
@@ -51,6 +44,7 @@ SECRET_PREFIX = "project/cia/smart-scheduling"
 SECRET_NAMES = [
     "destination.account_info",
 ]
+
 
 def inject_secrets(config):
     """
@@ -72,7 +66,6 @@ def inject_secrets(config):
         set_default(config, acc)
 
 
-
 class Schedulers:
     def __init__(self, config):
         self.config = config = to_data(config)
@@ -81,9 +74,7 @@ class Schedulers:
         config.start = Date(config.start)
         config.interval = Duration(config.interval)
         config.branches = listwrap(config.branches)
-        self.destination = bigquery.Dataset(config.destination).get_or_create_table(
-            config.destination
-        )
+        self.destination = bigquery.Dataset(config.destination).get_or_create_table(config.destination)
 
         # CALCULATE THE PREVIOUS RUN
         mozci_version = self.version("mozci")
@@ -111,7 +102,9 @@ class Schedulers:
             return None
 
     def set_state(self):
-        adr_configuration.config.cache.put(CACHY_STATE, value2json(self.done), minutes=CACHY_RETENTION)
+        adr_configuration.config.cache.put(
+            CACHY_STATE, value2json(self.done), minutes=CACHY_RETENTION
+        )
 
     def version(self, package):
         with Process("", ["pip", "show", package]) as p:
@@ -128,37 +121,50 @@ class Schedulers:
             json={
                 "from": "task",
                 "select": [
-                    {"name":"id", "value":"task.id"},
+                    {"name": "id", "value": "task.id"},
                     {"name": "label", "value": "run.name"},
                     {"name": "start_time", "value": "task.run.start_time"},
                     {"name": "end_time", "value": "task.run.end_time"},
                 ],
-                "where": [{"eq": {"repo.push.id": push.id}}, {"eq": {"repo.branch.name": push.branch}}],
+                "where": [
+                    {"eq": {"repo.push.id": push.id}},
+                    {"eq": {"repo.branch.name": push.branch}},
+                ],
                 "format": "list",
-                "limit": 100000
-            }
+                "limit": 100000,
+            },
         )
 
         tasks = {task.id: task for task in task_timings.data}
 
-        for _, tasks_ in jx.chunk((t for t in push.tasks if t.label.startswith("test-")), 10):
+        for _, tasks_ in jx.chunk(
+            (t for t in push.tasks if t.label.startswith("test-")), 10
+        ):
             chunk_of_tasks_timings = http.get_json(
                 self.config.adr.url,
                 timeout=60 * 5,
                 json={
                     "from": "unittest",
                     "select": [
-                        {"name": "start_time", "value": "action.start_time", "aggregate": "min"},
-                        {"name": "end_time", "value": "action.end_time", "aggregate": "max"}
+                        {
+                            "name": "start_time",
+                            "value": "action.start_time",
+                            "aggregate": "min",
+                        },
+                        {
+                            "name": "end_time",
+                            "value": "action.end_time",
+                            "aggregate": "max",
+                        },
                     ],
                     "groupby": [
-                        {"name":"task_id", "value":"task.id"},
-                        {"name": "name", "value": "result.group"}
+                        {"name": "task_id", "value": "task.id"},
+                        {"name": "name", "value": "result.group"},
                     ],
                     "where": [{"in": {"task.id": tasks_.id}}],
                     "format": "list",
-                    "limit": 10000
-                }
+                    "limit": 10000,
+                },
             )
             for g, groups in jx.groupby(chunk_of_tasks_timings.data, "task_id"):
                 task = tasks[g.task_id]
@@ -167,8 +173,7 @@ class Schedulers:
 
         return list(tasks.values())
 
-
-    def process_chunk(self, start, end, branch, please_stop):
+    def process_chunk_of_pushes(self, start, end, branch, please_stop):
         """
         :param start: begin time range
         :param end: end time range
@@ -218,14 +223,10 @@ class Schedulers:
                     scheduler = []
                     for s in schedulers:
                         try:
-                            scheduler.append(
-                                {
-                                    "name": s,
-                                    "tasks": jx.sort(
-                                        push.get_shadow_scheduler_tasks(s)
-                                    ),
-                                }
-                            )
+                            scheduler.append({
+                                "name": s,
+                                "tasks": jx.sort(push.get_shadow_scheduler_tasks(s)),
+                            })
                         except Exception:
                             pass
                 try:
@@ -239,26 +240,19 @@ class Schedulers:
                 tasks = self.get_task_details(push)
 
                 # RECORD THE PUSH
-                data.append(
-                    {
-                        "push": {
-                            "id": push.id,
-                            "date": push.date,
-                            "changesets": push.revs,
-                            "backedoutby": push.backedoutby,
-                        },
-                        "tasks": tasks,
-                        "schedulers": scheduler,
-                        "regressions": [
-                            {"label": name} for name in jx.sort(regressions)
-                        ],
-                        "branch": branch,
-                        "etl": {
-                            "revision": git.get_revision(),
-                            "timestamp": Date.now(),
-                        },
-                    }
-                )
+                data.append({
+                    "push": {
+                        "id": push.id,
+                        "date": push.date,
+                        "changesets": push.revs,
+                        "backedoutby": push.backedoutby,
+                    },
+                    "tasks": tasks,
+                    "schedulers": scheduler,
+                    "regressions": [{"label": name} for name in jx.sort(regressions)],
+                    "branch": branch,
+                    "etl": {"revision": git.get_revision(), "timestamp": Date.now(),},
+                })
         finally:
             # ADD WHATEVER WE HAVE
             with Timer("adding {{num}} records to bigquery", {"num": len(data)}):
@@ -291,7 +285,7 @@ class Schedulers:
             for start, end, branch in self.todo:
                 if please_stop:
                     break
-                self.process_chunk(start, end, branch, please_stop)
+                self.process_chunk_of_pushes(start, end, branch, please_stop)
         except Exception as e:
             Log.warning("Could not complete the etl", cause=e)
         else:
@@ -311,7 +305,7 @@ def main():
         capture_loguru()
 
         if config.taskcluster:
-           inject_secrets(config)
+            inject_secrets(config)
 
         @extend(SourceHandler)
         def update(self, sources):
@@ -329,9 +323,9 @@ def main():
                 if v != None:
                     self._config[k] = v
 
-            self._config["sources"] = sorted(
-                map(os.path.expanduser, set(self._config["sources"]))
-            )
+            self._config["sources"] = sorted(map(
+                os.path.expanduser, set(self._config["sources"])
+            ))
 
             # Use the NullStore by default. This allows us to control whether
             # caching is enabled or not at runtime.
@@ -341,12 +335,12 @@ def main():
                 if store.path and not store.path.endswith("/"):
                     # REQUIRED, OTHERWISE FileStore._create_cache_directory() WILL LOOK AT PARENT DIRECTORY
                     store.path = store.path + "/"
-            with Timer("setup adr source of {{path}}", {"path":config.sources}):
+            with Timer("setup adr source of {{path}}", {"path": config.sources}):
                 adr.sources.update(config.sources)
-
 
         if SHOW_S3_CACHE_HIT:
             s3_get = S3Store._get
+
             @extend(S3Store)
             def _get(self, key):
                 with Timer("get {{key}} from S3", {"key": key}, verbose=False) as timer:
